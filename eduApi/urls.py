334b21b4b5c671b3cc5a4430c3f80eb46de568f5
Gurjeet Singh <gurjeets@smartdatainc.net>
2018-03-07 12:19:54 +0530

    * added support for the Swagger documentation.

--- eduApi/urls.py
+++ eduApi/urls.py
@@ -1,9 +1,14 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Edu API')
 
 urlpatterns = [
     path(r'api/', include('universities.urls')),
 
     # Developer Tools
     path('admin/', admin.site.urls),
+    url(r'^$', schema_view),
 ]
